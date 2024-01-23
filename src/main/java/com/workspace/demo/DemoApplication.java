6b246992adafa7a1ce12e76510e115511e2baa91
Rahul-CMW <rahul@cybermindworks.com>
2024-01-23 18:14:36 +0530

    HTML file added with thymleaf

--- src/main/java/com/workspace/demo/DemoApplication.java
+++ src/main/java/com/workspace/demo/DemoApplication.java
@@ -1,17 +1,17 @@
 package com.workspace.demo;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.boot.SpringApplication;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "home";
     }
 
     public static void main(String[] args) {
